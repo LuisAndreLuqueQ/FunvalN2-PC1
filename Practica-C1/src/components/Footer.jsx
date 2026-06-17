4fa15ba4d4022e76b944ad93609e57b471ff6217
LuisAndreLuqueQ <l.luque.work@gmail.com>
2026-06-16 23:58:17 -0500

    Agregar lógica dinámica react, datos del equipo, componentes Cards y Equipo

---
+++ Practica-C1/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer(){
+
+    return(
+
+        <>
+    
+        <p className="m-5 pt-10 flex items-center justify-center">created by <span className="text-sm font-bold"> joregesosa </span> - devChallenges.io</p>
+
+    
+        </>
+        
+    )
+}
+
+export default Footer
